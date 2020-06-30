3726a74baffdb917a7be5ac2afc2873fd808466e
Odd Stråbø <oddstr13@openshell.no>
2020-06-30 15:00:04 +0200

    Update for 10.6.0

--- Jellyfin.Plugin.Pushover/Notifier.cs
+++ Jellyfin.Plugin.Pushover/Notifier.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Jellyfin.Plugin.Pushover.Configuration;
+using Jellyfin.Data.Entities;
 using MediaBrowser.Common.Net;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Notifications;
@@ -32,7 +33,7 @@ namespace Jellyfin.Plugin.Pushover
             return options != null && IsValid(options) && options.Enabled;
         }
 
-        private static PushOverOptions GetOptions(BaseItem user)
+        private static PushOverOptions GetOptions(User user)
         {
             return Plugin.Instance.Configuration.Options
                 .FirstOrDefault(i => string.Equals(i.UserId, user.Id.ToString("N"), StringComparison.OrdinalIgnoreCase));
